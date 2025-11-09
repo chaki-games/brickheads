f3bc74c741cc8048d26dbf79fccf7c03242e5f85
Ricardo Ramirez <ricardoramirezcaero123@gmail.com>
2025-11-09 12:21:57 -0400

    feat(player): add movement to player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,22 @@
 extends Area2D
+
+@export var speed = 500
+
+func _ready():
+	print("player ready")
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("IDLE")
+	
+	position += velocity * delta
